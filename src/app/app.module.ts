9b876446630456f857351d50d48d5bd60c9f41d8
keyurlakhani <keyur.lakhani99@gmail.com>
2018-04-18 15:54:26 -0700

    Template Driven Form Example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    ContactFormComponent
+  ],  
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: ContactFormComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
